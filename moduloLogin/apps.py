1b1bdba899af554b4e8ee12b4d8ea652473be353
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2024-08-29 17:09:35 -0300

    Delete moduloLogin directory

--- moduloLogin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModulologinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'moduloLogin'
--- moduloLogin/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Empleado
-
-class EmpleadoForm(forms.ModelForm):
-    fecha_contratacion = forms.DateField(required=True, label="Fecha de Contratación", widget=forms.SelectDateWidget)
-
-    class Meta:
-        model = Empleado
-        fields = ['dni', 'cuil', 'nombre', 'apellido', 'fecha_nacimiento', 'zona_asignada', 'sueldo', 'estado', 'telefono', 'seniority', 'annos_experiencia', 'fecha_inicio', 'rol', 'fecha_contratacion']
